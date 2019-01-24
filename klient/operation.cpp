029e52ca9afc6248fb556031e0f49ee6131cdf72
azablock <morlin1>
2019-01-24 16:34:06 +0100

    open msg request opendir

--- klient/operation.cpp
+++ klient/operation.cpp
@@ -32,3 +32,24 @@ int mynfs_read(int fd, void *buf, int count)
     return res_message->buf_len;
     //return descriptor;
 }
+
+int mynfs_readdir(int fd, void *buf, int count) {
+    RequestOperation req_message;
+    ResponseOperation *res_message;
+    char response[4096];
+    req_message.msg_id = MSG_REQUEST_OPERATION;
+    req_message.function_id = OPERATION_MSG_REQUEST_READDIR;
+    req_message.descriptor = htonl(fd);
+    req_message.token = htonl(global_token);
+    req_message.count = count;
+
+    send_message_and_wait_for_response((char*)global_host.c_str(), (uint16_t)global_port, (uint8_t *) &req_message, sizeof(req_message), response);
+
+    res_message = (ResponseOperation *) response;
+
+    std::cout << "RECEIVED READDIR RESPONSE - NICE ! " << std::endl;
+    std::cout<<res_message->buf << std::endl;
+
+    memcpy(buf, res_message->buf, res_message->buf_len);
+    return res_message->buf_len;
+}
