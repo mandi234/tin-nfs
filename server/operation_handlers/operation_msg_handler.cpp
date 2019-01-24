49a1cdf813cd7b9aedeade819e36d8dc38e722fb
root <root@hitler>
2019-01-25 00:49:32 +0100

    remove unnecessary function

--- server/operation_handlers/operation_msg_handler.cpp
+++ server/operation_handlers/operation_msg_handler.cpp
@@ -34,23 +34,6 @@ void OperationMsgHandler::readFile(RequestOperation *request_operation, int len,
     *resp = (char *) response_operation;
 }
 
-void OperationMsgHandler::readDir(RequestOperation *request_operation, int len, char **resp, int *response_len) {
-    auto *response_operation = (ResponseOperation *) malloc(sizeof(ResponseOperation));
-    auto dirDescriptor = ntohl(request_operation->descriptor);
-    auto dir = fdopendir(dirDescriptor);
-    std::string dirContent;
-    struct dirent *entry;
-
-    while ((entry = readdir(dir)) != nullptr) {
-        dirContent.append(entry->d_name).append("\n");
-    }
-
-    memcpy(response_operation->buf, dirContent.c_str(), dirContent.length());
-    response_operation->buf_len = static_cast<uint32_t>(dirContent.length());
-
-    *response_len = sizeof(ResponseOpen);
-    *resp = (char *) response_operation;
-}
 
 void OperationMsgHandler::writeFile(RequestOperation *request_operation, int len, char **resp, int *response_len) {
     int fd = ntohl(request_operation->descriptor);
